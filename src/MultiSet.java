2a228c0dbd032c7859b2963858ade4a25e30200f
Canberk Soytekin <csoytek05@hotmail.com>
2024-09-16 20:41:56 -0400

    Multiset Class Defined

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -9,6 +9,11 @@ public abstract class MultiSet {
      * @param item the item to add
      */
     abstract void add(Integer item);
+    abstract void remove(Integer item);
+    abstract void contains(Integer item);
+    abstract void is_empty(Integer item);
+    abstract void count(Integer item);
+    abstract void size(Integer item);
 
     // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
 }
